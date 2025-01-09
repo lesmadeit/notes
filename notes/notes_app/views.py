ae6b34728f0da81528ff4503bf23b36f7cb7e0a5
lesmadeit <lesmwendwa@gmail.com>
2025-01-09 13:36:10 +0300

    app

--- notes/notes_app/views.py
+++ notes/notes_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import *
 
-# Create your views here.
+def home(request):
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    notes = Note.objects.filter(Q(title__icontains=q)).order_by("-updated", "-created")
+    context = {"notes": notes}
+    return render(request, "notes_app/home.html", context)
