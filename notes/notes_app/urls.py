1eaf3abc9cb767ef72109eb8d4722252b412e585
lesmadeit <lesmwendwa@gmail.com>
2025-01-12 22:44:52 +0300

    urls

--- notes/notes_app/urls.py
+++ notes/notes_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", home, name="home")
+    path("", home, name="home"),
+    path("add/", add_note, name="add-note"),
+    path("delete/<str:pk>", delete_note, name="delete-note"),
+    path("edit/<str:pk>", edit_note, name="edit-note"),
+    path("display/<str:pk>/", display_note, name="display-note"),
 ]
